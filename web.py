03c5fa9f9a6bfc41fb9908cf2b18857896450814
Dong-Chen-1031 <dcdcdc1031@gmail.com>
2021-12-12 15:38:01 +0800

    fghjk

--- web.py
+++ web.py
@@ -4,20 +4,36 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-
 driver = webdriver.Chrome()
 driver.get('https://www.pagamo.org/')
 
-def cli(xpath):
-    WebDriverWait(driver, 20).until(
+def click(xpath):
+    WebDriverWait(driver, 100000).until(
             EC.presence_of_element_located((By.XPATH, xpath)))
     c = driver.find_element_by_xpath(xpath)
     c.click()
 
-cli('//*[@id="signin-button"]') # 註冊/登入
-cli('//*[@id="other-accounts-container"]/img[4]') # 小豆豆登入
-cli('/html/body/div[1]/div/div/div/div[2]/div[5]/button') # 使用縣市帳號登入
-cli('/html/body/div[1]/div/div/div/div[2]/div[6]/div/div/div[2]/div/div/div[3]') # 新北市政府教育局單一簽入服務
+def send(xpath,key):
+    WebDriverWait(driver, 100000).until(
+            EC.presence_of_element_located((By.XPATH, xpath)))
+    c = driver.find_element_by_xpath(xpath)
+    c.click()
+    c.clear()
+    c.send_keys(key)
 
-# time.sleep(100)
+
+click('//*[@id="signin-button"]') #註冊/登入
+click('//*[@id="other-accounts-container"]/img[4]') #小豆豆登入
+click('/html/body/div[1]/div/div/div/div[2]/div[5]/button') #使用縣市帳號登入
+time.sleep(3)
+click('//*[@id="id16"]/div/div/div[2]/div/div/div[3]/a') #新北市單一簽入
+send('/html/body/div[1]/div/div/div/div[2]/form/div[2]/input','lkes30501') #輸入帳號
+send('/html/body/div[1]/div/div/div/div[2]/form/div[3]/input','3.141592653589793') #輸入密碼
+time.sleep(10)
+click('/html/body/div[1]/div/div/div/div/div[3]/button') #您的教育雲端帳號是xxx
+
+while True:
+    click('/html/body/div[3]/div[3]/div[3]/div[2]/div[4]/div/div[3]/button')
+    time.sleep(3)
+
 
