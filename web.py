ab88d0755b1e66f5249a683a5fda27793978fca8
Dong-Chen-1031 <dcdcdc1031@gmail.com>
2021-12-12 17:55:37 +0800

    adfghjkl;'

--- web.py
+++ web.py
@@ -29,11 +29,23 @@ time.sleep(3)
 click('//*[@id="id16"]/div/div/div[2]/div/div/div[3]/a') #新北市單一簽入
 send('/html/body/div[1]/div/div/div/div[2]/form/div[2]/input','lkes30501') #輸入帳號
 send('/html/body/div[1]/div/div/div/div[2]/form/div[3]/input','3.141592653589793') #輸入密碼
-time.sleep(10)
-click('/html/body/div[1]/div/div/div/div/div[3]/button') #您的教育雲端帳號是xxx
+time.sleep(15)
+click('/html/body/div[1]/div/div/div/div/div[3]/button') #您的教育雲端帳號是xxx(我知道了！)
 
 while True:
-    click('/html/body/div[3]/div[3]/div[3]/div[2]/div[4]/div/div[3]/button')
+    click('/html/body/div[3]/div[3]/div[3]/div[2]/div[4]/div/div[3]/button') #攻擊
     time.sleep(3)
+    b1 = driver.find_element_by_xpath('/html/body/div[11]/div/div/div/div/div[3]/div/div[2]/div[1]')
+    if b1.get_attribute('class') == 'cursor_pointer choice-button attack_modal_sprite btn_4a1 pgo-style-input-answer-2o3iYm':
+        pass
+    # time.sleep(3)
+    # click('/html/body/div[11]/div/div/div/div/div[3]/div/div[2]/div[1]')
+    # time.sleep(1)
+    # click('/html/body/div[11]/div/div/div/div/div[3]/div/div[2]/div[2]')
+    # time.sleep(3)
+    # click('/html/body/div[11]/div/div/div/div/div[3]/div/div[2]/div[3]')
+    # time.sleep(1)
+    # click('/html/body/div[11]/div/div/div/div/div[3]/div/div[2]/div[4]')
+    
 
 
