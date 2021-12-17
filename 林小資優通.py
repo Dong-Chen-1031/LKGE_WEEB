a69c7eb8fada594514080defe276e7214b863819
Dong-Chen-1031 <dcdcdc1031@gmail.com>
2021-12-17 17:34:51 +0800

    kk

--- 林小資優通.py
+++ 林小資優通.py
@@ -1,5 +1,4 @@
 # 引用必要套件
-import socket
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
@@ -14,22 +13,20 @@ firebase_admin.initialize_app(cred)
 # 初始化firestore
 db = firestore.client()
 
-doc = {
-  '名稱':'',
-  '內容': '',
-  '準時':[False],
-  '班規':[False,False,False],
-  '作業': [False,False,False],
-  '教師簽章':None
-}
-
 # 建立文件 必須給定 集合名稱 文件id
 # 即使 集合一開始不存在 都可以直接使用
 
 # 語法
 # doc_ref = db.collection("集合名稱").document("文件id")
-
-doc_ref = db.collection("回報資料").document(socket.gethostname())
-
-# doc_ref提供一個set的方法，input必須是dictionary
-doc_ref.set(doc)
+for i in range(1,6,1):
+  doc = {
+  '名稱':'第'+str(i)+'堂',
+  '內容': str(i),
+  '準時':[False],
+  '班規':[False,False,False],
+  '作業': [False,False,False],
+  '教師簽章':None
+  }
+  doc_ref = db.collection("Dong Chen").document('第1周第'+str(i)+'堂')
+  # doc_ref提供一個set的方法，input必須是dictionary
+  doc_ref.set(doc)
