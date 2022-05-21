9dcfeb1308c174ac324863fbe0797a2f263819a6
Dong-home <dcdcdc1031@gmai.com>
2022-05-21 17:34:00 +0800

    e567890

---
+++ 其他/上線紀錄.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         上線紀錄
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  上線紀錄!柏東、浩雲專用
+// @author       Dong
+// @match        http://*/*
+// @icon         https://ssl.gstatic.com/ui/v1/icons/mail/images/favicon_chat_r2.ico
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    if (location.href.split("/")[2]='mail.google.com') {
+        console.log(location.href.split("/"));
+    }
+})();
