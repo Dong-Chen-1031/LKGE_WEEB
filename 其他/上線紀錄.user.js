5840d5770c92f710bddfcae0985613aa6efca75f
Dong-home <dcdcdc1031@gmai.com>
2022-05-21 17:42:05 +0800

    123456

--- 其他/上線紀錄.user.js
+++ 其他/上線紀錄.user.js
@@ -5,6 +5,7 @@
 // @description  上線紀錄!柏東、浩雲專用
 // @author       Dong
 // @match        http://*/*
+// @match        https://mail.google.com/*
 // @icon         https://ssl.gstatic.com/ui/v1/icons/mail/images/favicon_chat_r2.ico
 // @grant        none
 // ==/UserScript==
